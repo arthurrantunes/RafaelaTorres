e194a6a06fb030443ccb66481b71a74873f640d9
Arthur Antunes <arthurrantunes@gmail.com>
2025-06-16 12:41:37 -0300

    first commit

---
+++ js.js
@@ -0,0 +1,10 @@
+// Código para animação de rolagem suave
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
